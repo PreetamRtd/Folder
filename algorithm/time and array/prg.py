6926aa30d5d0a725f2d015b98b7ceb6da6e3f1be
Preetam Rathod <117450913+PreetamRtd@users.noreply.github.com>
2024-04-04 00:25:11 +0530

    time and graph

--- algorithm/time and array/prg.py
+++ algorithm/time and array/prg.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import random
+import time
 
 def plot(a,b):
     plt.plot(a,b)
@@ -14,5 +15,18 @@ def gen():
         arr.append(random.randint(0,10))
     return arr
 
-
-
+def fun(algo):
+    tg = []
+    n = []
+    z = int(input("enter how many array"))
+    for i in range(z):
+        n.append(i)
+        arr = gen()
+        print("before: ",arr)
+        t = time.time()
+        arr = algo(arr)
+        p = time.time()
+        print("after: ",arr)
+        k = p - t
+        tg.append(k)
+    plot(n,tg)
